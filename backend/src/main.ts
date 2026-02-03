66adbbce22c790f0fbf2d7a3495f642448c8ba20
tangara-aymen <aymen.tangara@playsoft.io>
2026-02-03 20:58:45 +0100

    Fix: Allow CORS for test url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000', 'http://127.0.0.1:3000'] });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://127.0.0.1:3000',
+      'http://action.playsoft.io',
+      'https://action.playsoft.io',
+    ],
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   const port = process.env.PORT ?? 4000;
   await app.listen(port);
